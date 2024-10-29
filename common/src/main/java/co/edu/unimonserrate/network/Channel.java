ca7a0a0c808bca3f6abe268d39c9e1222b2b66d5
srvenient <srvenient@gmail.com>
2024-10-29 14:28:54 -0500

    ✨ Add overloaded onEnable method in Channel interface for improved flexibility

--- common/src/main/java/co/edu/unimonserrate/network/Channel.java
+++ common/src/main/java/co/edu/unimonserrate/network/Channel.java
@@ -2,11 +2,14 @@ package co.edu.unimonserrate.network;
 
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public interface Channel {
-  void onEnable(final @NotNull JTextArea textArea) throws IOException;
+  void onEnable(final @NotNull String address, final int port) throws IOException;
+
+  default void onEnable(final int port) throws IOException {
+    this.onEnable("", port);
+  }
 
   void onDisable();
 }

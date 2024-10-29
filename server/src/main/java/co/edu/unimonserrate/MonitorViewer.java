07478dc95588ffe6d669cc011711fbc1378abf20
srvenient <srvenient@gmail.com>
2024-10-29 10:39:03 -0500

    🎨 Adds a new style to the console

--- server/src/main/java/co/edu/unimonserrate/MonitorViewer.java
+++ server/src/main/java/co/edu/unimonserrate/MonitorViewer.java
@@ -5,17 +5,34 @@ import co.edu.unimonserrate.network.ServerChannel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
 
 public final class MonitorViewer extends JFrame {
-
   public MonitorViewer(final @NotNull Channel channel) {
-    this.setTitle("Monitor Viewer");
-    this.setSize(800, 600);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setVisible(true);
+    super.setTitle("Monitor Viewer");
+    super.setSize(800, 600);
+    super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    super.setLocationRelativeTo(null);
+    super.setVisible(true);
 
     final var textArea = new JTextArea();
-    this.add(new JScrollPane(textArea));
+    textArea.setEditable(false);
+    textArea.setBackground(new Color(240, 240, 240));
+    textArea.setForeground(new Color(33, 33, 33));
+    textArea.setFont(new Font("Monospaced", Font.PLAIN, 15));
+    textArea.setLineWrap(true);
+    textArea.setWrapStyleWord(true);
+    textArea.setBorder(
+      BorderFactory.createCompoundBorder(
+        BorderFactory.createLineBorder(new Color(220, 220, 220), 1, true),
+        BorderFactory.createEmptyBorder(15, 15, 15, 15)
+      )
+    );
+
+    final var scrollPane = new JScrollPane(textArea);
+    scrollPane.setBorder(BorderFactory.createEmptyBorder());
+
+    super.add(scrollPane, BorderLayout.CENTER);
 
     try {
       channel.onEnable(textArea);
